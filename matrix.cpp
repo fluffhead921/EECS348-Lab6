5ac4289e371150874eaa42110a9b895b0b137760
Maya Routh <routhmaya@gmail.com>
2024-06-13 22:06:58 -0500

    added file input. reads matrix size from file

--- matrix.cpp
+++ matrix.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <string>
 
+using namespace std;
+
+int main() {
+    string fileName;
+    fstream inputFile;
+    string currLine;
+    int size;
+
+    //get file name and open
+    printf("Enter matrix file name: ");
+    cin >> fileName;
+    inputFile.open(fileName);
+
+    //get matrix size from first line of file
+    getline(inputFile, currLine);
+    size = stoi(currLine);
+
+    printf("Matrix Size: %d", size);
+
+    inputFile.close();
+}
